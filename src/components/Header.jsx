19f0dfa52b2b1c08d152210fd9c7c962237d2d6b
John Moen <entity.john@gmail.com>
2019-07-31 13:29:11 -0500

    style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,80 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { useAuthUserContext, useFirebaseContext } from '../firebase'
-import * as ROUTES from '../routes'
-
-const CurrentUserHUD = () => {
-	const firebaseApp = useFirebaseContext()
-	const { user } = useAuthUserContext()
-	return (
-		<div>
-			{user ? (
-				<>
-					<small>You are signed in as {user.email}</small>
-					<button onClick={firebaseApp.doSignOut}>Sign Out</button>
-				</>
-			) : (
-				<small>You are not signed in.</small>
-			)}
-		</div>
-	)
-}
-
+import styled from 'styled-components'
 export default function Header() {
 	return (
-		<header>
-			<CurrentUserHUD />
-			<Navigation />
-		</header>
+		<StyledHeader>
+			<MenuButton onClick={() => console.log('Open nav')}>&#9776; open</MenuButton>
+		</StyledHeader>
 	)
 }
 
-const Navigation = () => {
-	const { user } = useAuthUserContext()
-	const notSignedInCondition = !user
-	const signedInCondition = !!user
-	const adminCondition = user && user.userRole === `admin`
-
-	return (
-		<nav>
-			<ul>
-				{notSignedInCondition ? <NonAuthLinks /> : null}
-				{signedInCondition ? <AuthLinks /> : null}
-				{adminCondition ? <AdminLinks /> : null}
-			</ul>
-		</nav>
-	)
-}
-const NonAuthLinks = () => (
-	<>
-		<li>
-			<Link to={ROUTES.LANDING}>Home</Link>
-		</li>
-		<li>
-			<Link to={ROUTES.SIGNUP}>Sign Up</Link>
-		</li>
-		<li>
-			<Link to={ROUTES.SIGNIN}>Sign In</Link>
-		</li>
-	</>
-)
-const AuthLinks = () => (
-	<>
-		<li>
-			<Link to={ROUTES.USER_HOME}>User Home</Link>
-		</li>
-		<li>
-			<Link to={ROUTES.USER_ACCOUNT}>Account</Link>
-		</li>
-	</>
-)
-const AdminLinks = () => (
-	<>
-		<li>
-			<Link to={ROUTES.ADMIN}>Admin</Link>
-		</li>
-		<li>
-			<Link to={ROUTES.ADMIN_USER_LIST}>Admin</Link>
-		</li>
-	</>
-)
+const StyledHeader = styled.header`
+	min-height: 100px;
+	background-color: #888888;
+`
+const MenuButton = styled.span`
+	font-size: 30px;
+	cursor: pointer;
+`
